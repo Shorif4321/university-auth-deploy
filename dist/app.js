48def7d9e8789a68057850e4c7c633ff4ed46831
Shorif4321 <shorifali4321@gmail.com>
2024-10-08 13:14:00 +0600

    chage production

--- dist/app.js
+++ dist/app.js
@@ -22,7 +22,7 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-// // application routes
+// application routes
 // app.use('/api/v1/users', UserRoutes);
 // app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
 app.use('/api/v1/', routes_1.default);
--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// // application routes
+// application routes
 // app.use('/api/v1/users', UserRoutes);
 // app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
 app.use('/api/v1/', routers);
